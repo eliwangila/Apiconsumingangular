9876f977bd5940b1941f83ea803ee3f0d4f45c26
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-27 19:17:17 +0300

    hiding database credentials

--- BlogWebsiteAPI/BlogWebsiteAPI/settings.py
+++ BlogWebsiteAPI/BlogWebsiteAPI/settings.py
@@ -85,15 +85,7 @@ WSGI_APPLICATION = 'BlogWebsiteAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogwebsite',
-        'USER': 'ekirapa',
-        'PASSWORD': '99405897',
-        # 'HOST': 'localhost',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
